f737ea17f2dc0fe2ee590ee25da58cfaf2a4b811
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2026-01-06 14:10:17 +0500

    done

--- product-app/src/reduxconfig/store/store.js
+++ product-app/src/reduxconfig/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../reducers/cartSlice';
+import cartReducer from '../reducers/cartSlice.js';  
 
 const store = configureStore({
   reducer: {
